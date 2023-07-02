500d7e7e8ee402b2550c476a242672928b7e8299
fishillness <fishillness@gmail.com>
2023-07-02 19:33:10 +0600

    Script "Car" updated. The increase in speed is now more realistic.

--- Assets/Scripts/Car/Car.cs
+++ Assets/Scripts/Car/Car.cs
@@ -5,17 +5,23 @@ namespace Racing
     [RequireComponent(typeof(CarChassis))]
     public class Car : MonoBehaviour
     {
-        [SerializeField] private float maxMotorTorque;
         [SerializeField] private float maxSteerAngle;
         [SerializeField] private float maxBrakeTorque;
+
+        [SerializeField] private AnimationCurve engineTorqueCurve;
+        [SerializeField] private float maxMotorTorque;
+        [SerializeField] private float maxSpeed;
 
         private CarChassis chassis;
 
         [Header("DEBUG")]
+        public float linearVelocity;
         public float ThrottleControl;
         public float SteerControl;
         public float BrakeControl;
         //public float HandBrakeControl;
+
+        public float LinearVelocity => chassis.LinearVelocity;
 
         private void Start()
         {
@@ -24,7 +30,14 @@ namespace Racing
 
         private void Update()
         {
-            chassis.MotorTorque = maxMotorTorque * ThrottleControl;
+            //DEBUG
+            linearVelocity = LinearVelocity;
+
+            float engineTorque = engineTorqueCurve.Evaluate(LinearVelocity / maxSpeed) * maxMotorTorque;
+            if (LinearVelocity >= maxSpeed)
+                engineTorque = 0;
+
+            chassis.MotorTorque = engineTorque * ThrottleControl;
             chassis.SteerAngle = maxSteerAngle * SteerControl;
             chassis.BrakeTorque = maxBrakeTorque * BrakeControl;
         }
