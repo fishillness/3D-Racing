fe7bf89ccb107171f661608c5efc55df9284d19f
fishillness <fishillness@gmail.com>
2023-07-05 22:46:39 +0600

    Script "WheelEffect" added. Tires now leave skid marks.

---
+++ Assets/Scripts/Car/WheelEffect.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+namespace Racing
+{
+    public class WheelEffect : MonoBehaviour
+    {
+        [SerializeField] private GameObject skidPrefab;
+        [SerializeField] private WheelCollider[] wheels;
+
+        [SerializeField] private float forwardSlipLimit;
+        [SerializeField] private float sidewaySlipLimit;
+
+        private WheelHit wheelHit;
+        private Transform[] skidTrail;
+
+        private void Start()
+        {
+            skidTrail = new Transform[wheels.Length];
+        }
+
+        private void Update()
+        {
+            for (int i = 0; i < wheels.Length; i++)
+            {
+                wheels[i].GetGroundHit(out wheelHit);
+
+                if (wheels[i].isGrounded == true)
+                {
+                    if (wheelHit.forwardSlip > forwardSlipLimit ||
+                        wheelHit.sidewaysSlip > sidewaySlipLimit)
+                    {
+                        if (skidTrail[i] == null)
+                            skidTrail[i] = Instantiate(skidPrefab).transform;
+
+                        if (skidTrail[i] != null)
+                        {
+                            skidTrail[i].position = wheels[i].transform.position -
+                                wheelHit.normal * wheels[i].radius;
+                            skidTrail[i].forward = -wheelHit.normal;
+                        }
+
+                        continue;
+                    }
+                }
+
+                skidTrail[i] = null;
+            }
+        }
+    }
+}
