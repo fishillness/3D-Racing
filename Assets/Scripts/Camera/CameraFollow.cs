d58019a54c6badff1f087daa7a6c9b3467a24436
fishillness <fishillness@gmail.com>
2023-07-06 16:24:49 +0600

    Script "CameraFollow" added. The camera now follows the car smoothly.

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+namespace Racing
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        [SerializeField] private Transform target;
+        [SerializeField] private new Rigidbody rigidbody;
+
+        [Header("Offset")]
+        [SerializeField] private float viewHeight;
+        [SerializeField] private float height;
+        [SerializeField] private float distance;
+
+        [Header("Damping")]
+        [SerializeField] private float rotationDamping;
+        [SerializeField] private float heightDamping;
+        [SerializeField] private float SpeedThreshold;
+
+        private void FixedUpdate()
+        {
+            Vector3 velocity = rigidbody.velocity;
+            Vector3 targetRotation = target.eulerAngles;
+
+            if (velocity.magnitude > SpeedThreshold)
+            {
+                targetRotation = Quaternion.LookRotation(velocity, Vector3.up).eulerAngles;
+            }
+
+            //Lerp
+            float currentAngle = Mathf.LerpAngle(transform.eulerAngles.y,
+                targetRotation.y, rotationDamping * Time.fixedDeltaTime);
+            float currentHeight = Mathf.Lerp(transform.position.y,
+                target.position.y + height,
+                heightDamping * Time.fixedDeltaTime);
+
+            // Position
+            Vector3 positionOffset = Quaternion.Euler(0, currentAngle, 0) *
+                Vector3.forward * distance;
+            transform.position = target.position - positionOffset;
+            transform.position = new Vector3(transform.position.x, currentHeight,
+                transform.position.z);
+
+            // Rotation
+            transform.LookAt(target.position + new Vector3(0, viewHeight, 0));
+        }
+    }
+}
+
+
