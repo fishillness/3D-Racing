b6210aec9f33090d0e8867b0cdd22fc6fa1c72b1
fishillness <fishillness@gmail.com>
2023-08-24 15:56:26 +0600

    Script "UISelectableContainer" added.

---
+++ Assets/Scripts/UI/Buttons/Base/UISelectableContainer.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Racing
+{
+    public class UISelectableContainer : MonoBehaviour
+    {
+        [SerializeField] private Transform buttonsContainter;
+
+        private UISelectableButton[] buttons;
+        private int selectButtonIndex = 0;
+
+        private bool Interactable = true;
+        public bool SetInteractable(bool interactable) => Interactable = interactable;
+
+        private void Start()
+        {
+            buttons = buttonsContainter.GetComponentsInChildren<UISelectableButton>();
+
+            if (buttons == null)
+                Debug.LogError("Button lit i empty.");
+
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                buttons[i].PointerEnter += OnPointerEnter;
+            }
+
+            if (Interactable == false) return;
+
+            buttons[selectButtonIndex].SetFocuse();
+        }
+
+        private void OnDestroy()
+        {
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                buttons[i].PointerEnter -= OnPointerEnter;
+            }
+        }
+
+        private void OnPointerEnter(UIButton button)
+        {
+            SelectButton(button);
+        }
+
+
+        private void SelectButton(UIButton button)
+        {
+            if (Interactable == false) return;
+
+            buttons[selectButtonIndex].SetUnFocuse();
+
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                if (button == buttons[i])
+                {
+                    selectButtonIndex = i;
+                    button.SetFocuse();
+                    break;
+                }
+            }
+        }
+
+
+
+        /*
+        //TODO: сделать управление с клавиатуры:
+        public void SelectNext() { }
+
+        public void SelectPrecious() { }
+        */
+    }
+}
+
