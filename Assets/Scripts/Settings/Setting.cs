45b6facbc1e9c68fb4fa3526349201f45fef3da9
fishillness <fishillness@gmail.com>
2023-08-29 17:26:56 +0600

    Script "Settings" added.

---
+++ Assets/Scripts/Settings/Setting.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace Racing
+{
+    public abstract class Setting : ScriptableObject
+    {
+        [SerializeField] protected string title;
+        public string Title => title;
+
+        public virtual bool IsMinValue { get; }
+        public virtual bool IsMaxValue { get; }
+
+        public virtual void SetNextValue() { }
+        public virtual void SetPreviousValue() { }
+        public virtual object GetValue() { return default(object); }
+        public virtual string GetStringValue() { return string.Empty; } 
+    }
+}
