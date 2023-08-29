f0b281a2ffd231e3e83d986ddb4c072a029f2ac1
fishillness <fishillness@gmail.com>
2023-08-29 23:40:58 +0600

    Script "AudioMixerFloatSettingSetting" added. Now you can change the volume of music and sounds in the settings.

---
+++ Assets/Scripts/Settings/AudioMixerFloatSettingSetting.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace Racing
+{
+    [CreateAssetMenu]
+    public class AudioMixerFloatSettingSetting : Setting
+    {
+        [SerializeField] private AudioMixer audioMixer;
+        [SerializeField] private string nameParameter;
+
+        // -80..0
+        [SerializeField] private float minRealValue;
+        [SerializeField] private float maxRealValue;
+        // 0..100
+        [SerializeField] private float virtualStep;
+        [SerializeField] private float minVirtualValue;
+        [SerializeField] private float maxVirtualValue;
+
+        private float currentValue = 0;
+        
+        public override bool IsMinValue { get => currentValue == minRealValue; }
+        public override bool IsMaxValue { get => currentValue == maxRealValue; }
+
+        public override void SetNextValue()
+        {
+            AddValue(Mathf.Abs(maxRealValue - minRealValue) / virtualStep);
+        }
+
+        public override void SetPreviousValue()
+        {
+            AddValue(-Mathf.Abs(maxRealValue - minRealValue) / virtualStep);
+        }
+
+        public override string GetStringValue()
+        {
+            return Mathf.Lerp(minVirtualValue, maxVirtualValue,
+                (currentValue - minRealValue) / (maxRealValue - minRealValue)).ToString();
+        }
+
+        public override object GetValue()
+        {
+            return currentValue;
+        }
+
+        public override void Apply()
+        {
+            audioMixer.SetFloat(nameParameter, currentValue);
+        }
+
+        private void AddValue(float value)
+        {
+            currentValue += value;
+            currentValue = Mathf.Clamp(currentValue, minRealValue, maxRealValue);
+        }
+
+    }
+}
+
